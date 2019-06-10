e71a66b54d22540bc10dd3c3f8e84c73714b7c74
Almaz <almazzzik74@yandex.ru>
2019-06-10 21:56:06 +0300

    Add project files.

---
+++ Remake4Lab/Matrix.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+
+namespace Remake4Lab
+{
+    class Matrix
+    {
+        private int[,] _Matrix { get; }
+        public List<int> Vertexes { get; set; }
+        public List<int> Output { get; }
+        private int N { get; }
+        public Matrix(int n)
+        {
+            N = n;
+            _Matrix = new int[N, N];
+            Vertexes = new List<int>();
+            Output = new List<int>();
+            SendVertex();
+        }
+
+        private void SendVertex()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                Vertexes.Add(i+1);
+            }
+        }
+
+        public void FillMatrix()
+        {
+            var rnd = new System.Random();
+            for(int i = 0; i < N; i++)
+            {
+                for(int j = 0; j < N; j++)
+                {
+                    if (i == j) { _Matrix[i, j] = 0; }
+                    else {
+                        _Matrix[i, j] = rnd.Next(0,2);
+                        _Matrix[j, i] = _Matrix[i, j];
+                    }
+                }
+            }
+        }
+        public void DisplayMatrix()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                
+                for (int j = 0; j < N; j++)
+                {
+                    System.Console.Write(_Matrix[i,j] + " ");
+                }
+                System.Console.WriteLine();
+            }
+        }
+        public int FindMaxVertex()
+        {
+            int result = 0;
+            int count = 0, 
+                max = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (_Matrix[i, j] > 0)
+                    {
+                        count++;
+                    }
+                }   
+                if(i == 0) { max = count; }
+                if (count > max)
+                {
+                    max = count;
+                    result = Vertexes[i];
+                }
+                System.Console.WriteLine($" кол. ребер: {count} вершины: {Vertexes[i]}");
+                count = 0;
+            }            
+            return result;
+        }
+    }
+}
---
+++ Remake4Lab/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remake4Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            var matrix = new Matrix(N);
+
+            matrix.FillMatrix();
+
+            matrix.DisplayMatrix();
+
+            var res = matrix.FindMaxVertex();
+
+            Console.WriteLine("Вершина(ы) с макс. степенью: " + res);
+            Console.ReadLine();
+        }
+    }
+}
